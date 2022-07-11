29a32659a86b32fd5b71cb55a5110d32a97d186e
gabe-morris <gabe.e.morris@gmail.com>
2022-07-11 17:20:29 -0500

    deps, started server, finished post/get all users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,25 @@
 // BUILD YOUR SERVER HERE
+const Users = require('./users/model')
+const express = require('express')
+const server = express()
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//[POST] a new user
+server.post('/api/users', (req,res) => {
+    let body = req.body;
+    if(body.name == null || body.bio == null){
+        res.status(400).json({message: 'Please provide name and bio for the user'})
+        return;
+    }
+    Users.insert(body).then(user => {
+        res.status(201).json(user)
+    });
+})
+//[GET] users
+server.get('/api/users',(req,res) =>{
+    Users.find().then(users => res.status(200).json(users));
+})
+//[GET] a specified user
+//[DELETE] a specified user
+//[PUT] an update in for a specified user
+module.exports = server; // EXPORT YOUR SERVER instead of {}
