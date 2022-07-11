a9ca50f76294f8d4fdccb8e2314ce1a16328b57e
gabe-morris <gabe.e.morris@gmail.com>
2022-07-11 17:35:23 -0500

    completed get for specific user id

--- api/server.js
+++ api/server.js
@@ -15,11 +15,29 @@ server.post('/api/users', (req,res) => {
         res.status(201).json(user)
     });
 })
-//[GET] users
+//[GET] all users
 server.get('/api/users',(req,res) =>{
-    Users.find().then(users => res.status(200).json(users));
+    Users.find().then(users => {
+        if(users == null) {
+            res.status(500).json({message: "the users information could not be retrieved"})
+            return;
+        }else{
+            res.status(200).json(users);
+        }
+    });
 })
 //[GET] a specified user
+server.get('/api/users/:id', (req,res) => {
+    const {id} = req.params;
+    Users.findById(id).then(user => {
+        if(user == null){
+            res.status(404).json({message: 'the user with the specified ID does not exist'})
+        }else{
+            res.status(201).json(user)
+        }
+    })
+})
 //[DELETE] a specified user
+server.delete('/api/users/:id')
 //[PUT] an update in for a specified user
 module.exports = server; // EXPORT YOUR SERVER instead of {}
